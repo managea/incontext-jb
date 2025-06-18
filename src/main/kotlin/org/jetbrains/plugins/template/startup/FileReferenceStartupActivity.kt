2c0ca9500efbbf2b16c4e67a7a38346ce454675b
managea <asanga.manage@auspost.com.au>
2025-06-18 14:38:28 +1000

    Refactor file reference handling to use module names instead of project names

--- src/main/kotlin/org/jetbrains/plugins/template/startup/FileReferenceStartupActivity.kt
+++ src/main/kotlin/org/jetbrains/plugins/template/startup/FileReferenceStartupActivity.kt
@@ -5,18 +5,16 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import org.jetbrains.plugins.template.navigation.FileReferenceIndexer
 
-/**
- * Startup activity to initialize the file reference indexer
- */
-class FileReferenceStartupActivity : StartupActivity {
-    
+class FileReferenceStartupActivity : com.intellij.openapi.startup.ProjectActivity {
+
     companion object {
         private val LOG = Logger.getInstance(FileReferenceStartupActivity::class.java)
     }
-    
-    override fun runActivity(project: Project) {
+
+
+    override suspend fun execute(project: Project) {
         LOG.debug("Initializing file reference indexer")
-        
+
         // Get the indexer service and index the project
         val indexer = FileReferenceIndexer.getInstance(project)
         indexer.indexProject()
